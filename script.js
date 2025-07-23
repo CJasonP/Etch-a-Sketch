24083c1bbe46b90c4caa9437b50bf712a606de21
CJasonP <jasonpurnomo12@gmail.com>
2025-07-23 23:03:14 +0700

    Added custom grid amount

--- script.js
+++ script.js
@@ -12,20 +12,37 @@ container.style.flexWrap = 'wrap';
 container.style.width = '640px';
 container.style.height = '640px';
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.style.width = '40px';
-    square.style.height = '40px';
-    square.style.boxSizing = 'border-box';
-    square.style.border = '1px solid #ccc';
-    square.style.background = '#fff';
-    square.addEventListener('mouseover', () => {
-        square.style.background = 'blue';
-    });
-    square.addEventListener('mouseout', () => {
-        setTimeout(() => {
-        square.style.background = '#fff';
-        }, 300);
-    });
-    container.appendChild(square);
+let smolbox = 0;
+
+
+function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        let smolbox = 640/size;
+        square.style.width = `${smolbox}px`; //640/16 jadi 640/size
+        square.style.height = `${smolbox}px`;
+        square.style.boxSizing = 'border-box';
+        square.style.border = '1px solid #ccc';
+        square.style.background = '#fff ';
+        square.addEventListener('mouseover', () => {
+            square.style.background = 'blue';
+        });
+        square.addEventListener('mouseout', () => {
+            setTimeout(() => {
+                square.style.background = '#fff';
+            }, 300);
+        });
+        container.appendChild(square);
+    }
 }
+
+createGrid(16); 
+
+button.addEventListener('click', () => {
+    container.innerHTML = ''; 
+    let size = prompt('Enter grid size: ');
+    if(size > 0 && size <= 100) {
+        createGrid(size);
+    }
+    else alert('Please enter a valid size between 1 and 100.');
+});
